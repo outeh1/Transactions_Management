928bc7ee6c356e77f09f1ef096ebff5d501775ff
outeh <outeh@outeh-pc>
2021-04-10 10:41:06 +0200

    Added basic construct for uebung2, intentional LostUpdates with 2 Threads

---
+++ TmUebung1/src/uebung2/Thread1.java
@@ -0,0 +1,12 @@
+package uebung2;
+import baseStuff.RunnableAdapter;
+
+public class Thread1 extends RunnableAdapter{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ TmUebung1/src/uebung2/Thread2.java
@@ -0,0 +1,12 @@
+package uebung2;
+import baseStuff.RunnableAdapter;
+
+public class Thread2 extends RunnableAdapter{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
