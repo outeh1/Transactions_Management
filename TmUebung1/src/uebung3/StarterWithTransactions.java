2bc2e70129b331863be78cd0ee6049c302aee806
outeh <outeh@outeh-pc>
2021-04-17 10:15:01 +0200

    -added uebung3 a,b,c -added DbStarter Skript for postgres and Mariadb

---
+++ TmUebung1/src/uebung3/StarterWithTransactions.java
@@ -0,0 +1,54 @@
+package uebung3;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StarterWithTransactions {
+	private static final Integer TransferThreadNum = 1;
+	private static final Integer ThreadTransferNum = 100;
+	private static final BigDecimal betrag = BigDecimal.valueOf(100, 0);
+
+	public static void main(String[] args) {
+		uebung1.Minimalbeispiel.toggleVerbose();
+		final String mysqliteURL = "jdbc:sqlite:helloFhdw.sqlite.db";
+		final String mariadbURL = "jdbc:mariadb://localhost:3306/test?user=outeh";
+		final String postgreSQLURL = "jdbc:postgresql://localhost/postgres?user=outeh";
+
+		final String[] urls = { mysqliteURL };
+		// final String[] urls = {postgreSQLURL,mariadbURL,mysqliteURL};
+		for (String url : urls) {
+			try (final Connection conn = DriverManager.getConnection(url)) {
+				System.out.println("Verbunden mit Datenbank" + url);
+				/*
+				 * CREATE TABLE "konten" ( "kto" INTEGER NOT NULL, "betrag" INTEGER NOT NULL,
+				 * "kunde" TEXT NOT NULL, PRIMARY KEY("kto") );
+				 */
+				uebung1.Minimalbeispiel.ResetTable(conn);
+				uebung1.Minimalbeispiel.kundenListing(conn);
+
+				List<uebung3.TransferThread> Threads = new ArrayList<uebung3.TransferThread>();
+				for (int i = 0; i < TransferThreadNum; i++) {
+					Threads.add(new uebung3.TransferThread(url, "A", "B", betrag, ThreadTransferNum));
+					Threads.add(new uebung3.TransferThread(url, "B", "C", betrag, ThreadTransferNum));
+					Threads.add(new uebung3.TransferThread(url, "C", "A", betrag, ThreadTransferNum));
+				}
+				for (uebung3.TransferThread thread : Threads) {
+					thread.start();
+				}
+				for (uebung3.TransferThread thread : Threads) {
+					thread.join();
+				}
+				uebung1.Minimalbeispiel.kundenListing(conn);
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+}
--- TmUebung1/src/uebung3/TransferThread.java
+++ TmUebung1/src/uebung3/TransferThread.java
@@ -1,4 +1,4 @@
-package uebung2;
+package uebung3;
 
 import java.math.BigDecimal;
 import java.sql.Connection;
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import baseStuff.RunnableAdapter;
 
 public class TransferThread extends RunnableAdapter {
-
 	private final String connectionString;
 	private final String sourceAccount;
 	private final String targetAccount;
@@ -28,12 +27,20 @@ public class TransferThread extends RunnableAdapter {
 	@Override
 	public void run() {
 		try (Connection conn = DriverManager.getConnection(this.connectionString)) {
+			conn.setAutoCommit(false);
 			for (int i = 0; i < this.TransferNum; i++) {
 				uebung1.Minimalbeispiel.bankTransfer(conn, this.sourceAccount, this.targetAccount, this.amount);
-				// this.delay(200);
+//				if (i % 2 == 0) {
+//					conn.rollback();
+//				} else {
+//					conn.commit();
+//				}
+				this.delay(200);
+				conn.commit();
 			}
-			// System.out.printf("%s hat Überweisungen ausgeführt.%n",
-			// this.getWhereIRunIn());
+
+			// conn.rollback();
+
 		} catch (SQLException e) {
 			ErrorNum++;
 			// e.printStackTrace();
@@ -41,5 +48,4 @@ public class TransferThread extends RunnableAdapter {
 		System.err.println("There where: " + ErrorNum + " Errors");
 
 	}
-
 }
